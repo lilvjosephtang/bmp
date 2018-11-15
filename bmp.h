478d673c5f90549315d4be14afcfb3d7ea93c820
lilvjosephtang <45066269+lilvjosephtang@users.noreply.github.com>
2018-11-15 19:10:17 +0800

    Add files via upload

---
+++ bmp.h
@@ -0,0 +1,41 @@
+
+// word1.h : main header file for the word1 application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+
+	#error "include 'stdafx.h' before including this file for PCH"
+
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// Cword1App:
+// See word1.cpp for the implementation of this class
+//
+
+class Cword1App : public CWinAppEx
+{
+public:
+	Cword1App();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cword1App theApp;
